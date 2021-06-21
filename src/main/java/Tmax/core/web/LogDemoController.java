a0b9ca2fb5919037a3b83da8a2bc15955df71d78
changhyuk woo <changhyuk_woo@tmax.co.kr>
2021-06-21 18:11:03 +0900

    feat: section 9 complete

---
+++ src/main/java/Tmax/core/web/LogDemoController.java
@@ -0,0 +1,30 @@
+package Tmax.core.web;
+import Tmax.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import javax.servlet.http.HttpServletRequest;
+
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger;
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) throws InterruptedException {
+        String requestURL = request.getRequestURL().toString();
+
+        System.out.println("myLogger.getClass() = " + myLogger.getClass());
+  //      MyLogger myLogger = myLoggerProvider.getObject();
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        Thread.sleep(1000);
+        logDemoService.logic("testId");
+        return "OK";
+    }
+}
