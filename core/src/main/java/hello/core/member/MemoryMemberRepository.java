e362707487e55b428296bb047b07d9fef90320d5
hyunwoo_yoo <hyunwoo_yoo@tmax.co.kr>
2021-07-20 14:51:45 +0900

    feat: section 2(chapter 16) complete

---
+++ core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,19 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>(); //동시성 이슈가 있을 수 있어 원래는 컨커런트 해쉬맵 써야함. 예제니까 간단하게 그냥 해쉬맵
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
